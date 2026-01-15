07101d0deef7c16d8e8bbbd9dcc9b93b171e53c3
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-15 08:52:50 +0200

    add user signup functionality with password hashing and validation.

--- routing/user-routes.js
+++ routing/user-routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getAllUsers } from "../controllers/user-controllers";
+import { getAllUsers, signup } from "../controllers/user-controllers";
 
 const userRouter = Router();
 
 userRouter.get("/", getAllUsers);
+userRouter.post("/signup", signup);
 
 export default userRouter;
