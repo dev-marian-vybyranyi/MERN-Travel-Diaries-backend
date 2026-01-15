0e4e46154ee0d027a645dc8cf1f2cb81a5a001a3
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-15 08:59:16 +0200

    Implement `addPost` controller and add a new POST route for creating posts.

--- routing/post-routes.js
+++ routing/post-routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getAllPosts } from "../controllers/post-controller";
+import { getAllPosts, addPost } from "../controllers/post-controller";
 
 const postRouter = Router();
 
 postRouter.get("/", getAllPosts);
+postRouter.post("/", addPost);
 
 export default postRouter;
